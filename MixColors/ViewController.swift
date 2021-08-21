8b7024a19918c5660d1d8123ce9f392e647c1716
Andrey Shustov <andrejshusto@mail.ru>
2021-08-21 22:39:00 +0300

    Implementing color mixing logic

--- MixColors/ViewController.swift
+++ MixColors/ViewController.swift
@@ -28,17 +28,29 @@ class ViewController: UIViewController {
         
     }
     
+    func changeColorView() {
+        mixColorView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
+    }
+    
     @IBAction func redSliderAction() {
         let roundSliderValue = round(redSlider.value * 100) / 100
         redCountSliderLabel.text = String(roundSliderValue)
+        changeColorView()
     }
     @IBAction func greenSliderAction() {
         let roundSliderValue = round(greenSlider.value * 100) / 100
         greenCountSliderLabel.text = String(roundSliderValue)
+        changeColorView()
     }
     @IBAction func blueSliderAction() {
         let roundSliderValue = round(blueSlider.value * 100) / 100
         blueCountSliderLabel.text = String(roundSliderValue)
+        changeColorView()
     }
 }
 
